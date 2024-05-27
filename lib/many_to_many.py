3f604165a2132f1419912025f8ce9c663c7f36be
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-27 18:38:46 +0300

    new

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,62 @@
+
+
 class Author:
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        new_contract = Contract(self, book, date, royalties)
+        return new_contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
     pass
 
 
+
+
+
 class Book:
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
     pass
 
 
 class Contract:
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Invalid author")
+        if not isinstance(book, Book):
+            raise Exception("Invalid book")
+        if not isinstance(date, str):
+            raise Exception("Invalid date")
+        if not isinstance(royalties, (int, float)):
+            raise Exception("Invalid royalties")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
     pass
